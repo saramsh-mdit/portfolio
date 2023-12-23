b55649534aafcd3245b80bfc3af36ca14e7e1107
saramsh stha <stha.saramsh.mdit@gmail.com>
2023-12-23 19:41:37 +0545

    Add:netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify/functions";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  adapter: netlify(),
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind(), react()],
 });
